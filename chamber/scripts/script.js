25823ae65b9b9ba53972629eb2fbadb1a18295a2
giuseppebrandi-byui <bra22041@byui.edu>
2023-06-01 16:56:00 +0100

    Added lazy loading and gallery images

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -46,3 +46,38 @@ if (today === 1 || today === 2) {
 } else {
   bannerEl.style.display = "none";
 }
+
+// Get all images with data-src attribute
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 900px 0px",
+};
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+// First check to see if Intersection Observer is supported
+if ("IntersectionObserver" in window) {
+  const imgObserver = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+  // Loop through each img and check status and load if necessary
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
