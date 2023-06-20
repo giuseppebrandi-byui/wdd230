230c0c824845df1d584a2e3dfe879cd0edfed14d
giuseppebrandi-byui <bra22041@byui.edu>
2023-06-20 09:44:01 +0100

    Added openweathermap API to porject

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,10 +1,42 @@
-const speedValue = document.querySelector(".speed-value span");
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=Skipton&appid=b8656f4827a8f597f04278ed574ffd6c&units=imperial";
+
 const tempValue = document.querySelector(".temperature span");
+const weatherIcon = document.querySelector(".weather-icon");
+const weatherDescription = document.querySelector(".weather-type");
+const speedValue = document.querySelector(".speed-value span");
 const chillValue = document.querySelector(".chill-value");
 
-const temperatureF = 30;
-const temperatureC = Math.round(((temperatureF - 32) * 5) / 9);
-const windSpeed = 5;
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(weatherData) {
+  const temperatureF = `${weatherData.main.temp}`;
+  const temperatureC = ((temperatureF - 32) * 5) / 9;
+  tempValue.innerHTML = `${temperatureC.toFixed(0)}`;
+  const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`;
+  const desc = weatherData.weather[0].description;
+  const windSpeed = weatherData.wind.speed.toFixed(1);
+  const windChill = calculateWindChill(temperatureF, windSpeed);
+  console.log(windChill);
+
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  weatherDescription.textContent = desc;
+  speedValue.textContent = windSpeed;
+  chillValue.innerHTML = windChill;
+}
 
 // Compute the wind chill value
 function calculateWindChill(temperatureF, windSpeed) {
@@ -24,8 +56,4 @@ function calculateWindChill(temperatureF, windSpeed) {
   }
 }
 
-// Invoke calculateWindChill function and pass temperatureF and windspeed
-const windChill = calculateWindChill(temperatureF, windSpeed);
-speedValue.textContent = windSpeed;
-tempValue.textContent = temperatureC;
-chillValue.innerHTML = windChill;
+apiFetch();
